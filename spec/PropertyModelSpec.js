7321f66014c93d4f6f823f24a1786f70214e690f
Pea M. Tyczynska <CrystalPea@users.noreply.github.com>
2017-02-19 14:57:19 +0000

    Proprty model attributes defined

--- spec/PropertyModelSpec.js
+++ spec/PropertyModelSpec.js
@@ -2,10 +2,18 @@ describe("Property", function() {
   var property;
 
   beforeEach(function() {
-    property = new Property({location: "London"});
+    property = new Property({location: "London", price: "Guide price £450,000", bedrooms: 1 });
   });
 
   it("should be created with location property", function() {
     expect(property._location).toEqual("London");
+  });
+
+  it("should be created with price property", function() {
+    expect(property._price).toEqual("Guide price £450,000");
+  });
+
+  it("should be created with bedrooms property", function() {
+    expect(property._bedrooms).toEqual(1);
   });
 });
--- src/PropertyModel.js
+++ src/PropertyModel.js
@@ -1,3 +1,5 @@
 function Property(hash) {
 this._location = hash.location;
+this._price = hash.price;
+this._bedrooms = hash.bedrooms;
 }

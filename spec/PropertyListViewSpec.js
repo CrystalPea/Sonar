304569bd9ba0f9e71d5df0beaeb5a6ac73713e43
Pea M. Tyczynska <CrystalPea@users.noreply.github.com>
2017-02-19 17:55:17 +0000

    PropertyListView lists properties with html wrappers

--- spec/PropertyListViewSpec.js
+++ spec/PropertyListViewSpec.js
@@ -5,24 +5,36 @@ describe("PropertyListView", function() {
   var property_2
 
   beforeEach(function() {
-    property_1 = jasmine.createSpyObj('property_1', ['returnLocation']);
+    property_1 = jasmine.createSpyObj('property_1', ['returnLocation', 'returnId']);
     property_1.returnLocation.and.callFake(function() {
       return "London";
     });
-    property_2 = jasmine.createSpyObj('property_2', ['returnLocation']);
+    property_1.returnId.and.callFake(function() {
+      return 12;
+    });
+    property_2 = jasmine.createSpyObj('property_2', ['returnLocation', 'returnId']);
     property_2.returnLocation.and.callFake(function() {
       return "Brighton";
     });
+    property_2.returnId.and.callFake(function() {
+      return 14;
+    });
     propertyList = jasmine.createSpyObj('propertyList', ['returnPropertyList']);
     propertyList.returnPropertyList.and.callFake(function() {
-      return "London";
+      return [property_1, property_2];
     });
     propertyListView = new PropertyListView(propertyList);
 });
 
-  describe("propertyList creation", function() {
+  describe("propertyListView creation", function() {
     it("should be created with a PropertyList instance", function() {
       expect(propertyListView._propertyList).toEqual(propertyList);
+    });
+  });
+  describe("viewing properties", function() {
+    it("list properties with HTML wrappers", function() {
+      var pattern = "<ul><li><a href=\"#12\">London</a></li><li><a href=\"#14\">Brighton</a></li></ul>"
+      expect(propertyListView.listProperties()).toEqual(pattern);
     });
   });
 });
--- src/views/PropertyListView.js
+++ src/views/PropertyListView.js
@@ -1,3 +1,12 @@
 function PropertyListView(propertyList) {
   this._propertyList = propertyList
 };
+
+PropertyListView.prototype.listProperties = function(){
+    properties = "<ul>";
+    this._propertyList.returnPropertyList().forEach(function(element){
+      properties += "<li><a href=\"#" + element.returnId() + "\">" + element.returnLocation() + "</a></li>";
+    });
+    properties += "</ul>";
+    return properties;
+  };
